5afec27fd58778127a8f3992cdc3d63d5fefef63
2004khushi <goyalkhushi1004@gmail.com>
2026-01-06 15:54:41 +0530

    homepage creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
+import {Navbar} from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -28,17 +29,18 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
+      <Navbar />
       <div className="absolute inset-0 top-0 z-[-1] min-h-screen ">
       <LightRays
           raysOrigin="top-center-offset"
           raysColor="#5dfeca"
           raysSpeed={0.9}
           lightSpread={0.9}
-          rayLength={0.4}
+          rayLength={0.9}
           followMouse={true}
           mouseInfluence={0.02}
           noiseAmount={0.0}
-          distortion={0.35}
+          distortion={0.55}
       />
       </div>
 
